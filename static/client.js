dbaf6aa0c70ee29a6e74fcbffcfd3d21c8b40311
alberto__segura <alberto.segura.delgado@gmail.com>
2015-07-19 13:27:13 +0200

    custom forum colors

--- static/client.js
+++ static/client.js
@@ -8,13 +8,30 @@
 
 	/* Este el único hook que he encontrado que se ejecuta lo suficientemente pronto sin pasarse, pero es genérico, por lo que comprobamos que estamos dentro de un Topic */
 	$(window).on('action:ajaxify.contentLoaded', function () {
-		if ($('.topic').length) {
+		// Siempre cargamos la personalizacion del usuario, si el topic tiene una porpia la sobreescribira luego
+		loadUserCustomization();
+		if ($('.topic').length){
 			init();
+		}
+		else
+		{
+			var $button = $('<button class="btn btn-sm btn-default customize-topic hidden-xs" title="Personalizar"><i class="fa fa-paint-brush fa-lg"></i></button>');
+			$('#content').append($button);
+			$button.tooltip({
+				placement: 'left'
+			});
+			$button.click(function () {
+				openUserCustomizeWindow();
+			});
 		}
 	});
 
 	var BACKGROUND_PROPERTY = 'linear-gradient(transparent, #F6F6F6), url("{url}") center center / cover transparent';
-	var options;
+	var options = {
+		brandColor: '',
+		headerImage: '',
+		hideTitle: false
+	};
 
 	function init() {
 
@@ -79,12 +96,24 @@
 
 		/* Reemplazamos todos los elementos que tengan el color del tema por defecto */
 		if (options.brandColor && options.brandColor.match(/^(#\w{3,6}|[a-z]+)$/i)) {
+			// Elementos de un topic
 			style.textContent += '.custom-topic .post-row .post-header {background: {color}; border-color: {color};}';
 			style.textContent += '.custom-topic a {color: {color}}';
 			style.textContent += '.custom-topic .username-field {color: {color} !important}';
 			style.textContent += '.custom-topic .post_reply span {background: {color}}';
 			style.textContent += '.custom-topic .pagination > .active > a {background: {color}; border-color: {color}; color: white; }';
 			style.textContent += '.custom-topic .pagination a, .custom-topic .pagination a:hover {color: {color};}';
+			
+			// Elementos del resto del foro
+			style.textContent += '.tag-topic-count {background: {color}; border-color: {color};}';
+			style.textContent += '.tag-item {background: {color}; border-color: {color};}';
+			style.textContent += '.category .category-topics .threadlisthead {background: {color}; border-color: {color};}';
+			style.textContent += '.pagination > .active > a {background: {color}; border-color: {color}; color: white; }';
+			style.textContent += '.pagination a, .pagination a:hover {color: {color};}';
+			style.textContent += 'body a {color: {color}}';
+			style.textContent += '.panel-default > .panel-heading {background: {color}; border-color: {color};}';
+			style.textContent += '.btn-exodo span {background: {color}}';
+
 			style.textContent = style.textContent.replace(/\{color\}/g, options.brandColor);
 		}
 
@@ -207,6 +236,156 @@
 					message: "Hubo un error al guardar! Asegurate de que sea una imagen y de que no exceda un tamaño de 300KB",
 					alert_id: 'customization_error'
 				});
+			}
+		});
+	}
+
+	/*
+		Carga las opciones de personalizacion del usuario
+		para el foro en general
+	*/
+	function getUserCustomization()
+	{
+		// Obtenemos la personalizacion
+		socket.emit('topics.getUserCustomization', {}, function (err, topicOptions) {
+			if (topicOptions) {
+				options = topicOptions;
+				options.headerImage = null;
+				options.brandColor = sanitize(options.brandColor);
+				loadCustomizations(options);
+				localStorage.setItem("userCustomization", JSON.stringify(options));
+			}
+		});
+	}
+	function loadUserCustomization()
+	{
+		if(!localStorage.userCustomization)
+		{
+			getUserCustomization();
+		}
+		else
+		{
+			options = JSON.parse(localStorage.userCustomization);
+			loadCustomizations(options);
+		}
+	}
+
+	/*
+		Guardar personalizacion de usuario
+	*/
+	function saveUserCustomizations(options) {
+		socket.emit('topics.saveUserCustomization', {
+			options: options
+		}, function(err, r){
+			if(!err)
+			{
+				app.alert({
+					type: 'success',
+					timeout: 3000,
+					title: 'Guardado!',
+					message: "Personalización guardada!",
+					alert_id: 'customization_ok'
+				});
+			}
+			else
+			{
+				app.alert({
+					type: 'danger',
+					timeout: 3000,
+					title: 'Error',
+					message: "Hubo un error al guardar!",
+					alert_id: 'customization_error'
+				});
+			}
+		});
+	}
+
+
+
+	/*
+		Mostrar dialogo para guardar y elegir personalizacion del foro
+	*/
+	function openUserCustomizeWindow() {
+		window.templates.parse('user_customizer', {
+			topic_title: "Titulo",
+			brand_color: options.brandColor || '',
+			hide_title: options.hideTitle || false
+		}, function (template) {
+
+			var dialog = buildUserCustomizationDialog(template);
+
+			var preview = dialog.find('.topic-preview');
+
+			if (options.headerImage) {
+				preview.find('.topic-preview-header').css('background', BACKGROUND_PROPERTY.replace('{url}', options.headerImage));
+			}
+			preview.find('.topic-preview-post-title').css('background', options.brandColor);
+
+			/*
+			dialog.find('#header-image-input').keyup(function () {
+				var $status = $('#header-image-loading > i');
+				var url = $(this).val();
+				preview.find('.topic-preview-header').css('background', BACKGROUND_PROPERTY.replace('{url}', ''));
+				if (!url) {
+					$status.attr('class', 'fa fa-globe');
+				} else if (url.match(/^https?:\/\/.+?\/.+?\.(png|jpg|jpeg|gif)$/)) {
+					$status.attr('class', 'fa fa-spin fa-refresh');
+					var bgImg = new Image();
+					bgImg.onload = function () {
+						$status.attr('class', 'fa fa-check');
+						preview.find('.topic-preview-header').css('background', BACKGROUND_PROPERTY.replace('{url}', url));
+					};
+					bgImg.src = url;
+				} else {
+					$status.attr('class', 'fa fa-close');
+				}
+			});
+			*/
+
+			dialog.find('#brand-color-input').keyup(function () {
+				var color = $(this).val();
+
+				if (color && color.match(/^(#\w{3,6}|[a-z]+)$/i)) {
+					preview.find('.topic-preview-post-title').css('background', color);
+				} else {
+					preview.find('.topic-preview-post-title').css('background', '');
+				}
+			});
+
+			dialog.find('#hide-title-check').change(function () {
+				if ($(this).get(0).checked) {
+					preview.find('.topic-preview-title').addClass('hide');
+				} else {
+					preview.find('.topic-preview-title').removeClass('hide');
+				}
+			});
+		});
+	}
+
+	function buildUserCustomizationDialog(template) {
+		return bootbox.dialog({
+			title: 'Personalizar',
+			message: template,
+			buttons: {
+				cancel: {
+					label: 'Cancelar',
+					className: 'btn-default',
+					callback: function (e) {
+						getUserCustomization();
+						return true;
+					}
+				},
+				save: {
+					label: 'Aceptar',
+					className: 'btn-primary',
+					callback: function (e) {
+						options.headerImage = sanitize($('#header-image-input').val());
+						options.brandColor = sanitize($('#brand-color-input').val());
+						options.hideTitle = $('#hide-title-check').get(0).checked;
+						saveUserCustomizations(options);
+						return getUserCustomization();
+					}
+				}
 			}
 		});
 	}
---
+++ static/templates/user_customizer.tpl
@@ -0,0 +1,27 @@
+<p class="text-center">
+	Mejora el aspecto del foro personalizando la cabecera y los colores del mismo.
+</p>
+<hr>
+
+<div class="topic-preview">
+	<div class="topic-preview-header">
+		<h4 class="topic-preview-title <!-- IF hide_title -->hide<!-- ENDIF hide_title -->">{topic_title}</h4>
+		<div class="topic-preview-post">
+			<div class="topic-preview-post-title"></div>
+		</div>
+	</div>
+</div>
+
+<h4>Opciones</h4>
+
+<div class="form-group">
+	<div class="checkbox" style="padding: 0;">
+		<input type="checkbox" style="margin: 2px 4px 0px 0px;" id="hide-title-check" <!-- IF hide_title -->checked<!-- ENDIF hide_title -->/> Ocultar t&iacute;tulo del hilo
+	</div>
+</div>
+
+<div class="form-group">
+	<label for="brand-color-input">Color principal</label>
+	<input type="color" value="{brand_color}" class="form-control" id="brand-color-input" placeholder="Ej. #CF246A">
+	<span class="help-block">Color que se aplicar&aacute; a botones y posts</span>
+</div>
